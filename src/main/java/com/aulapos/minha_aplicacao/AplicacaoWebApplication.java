204b2fea7100449ccf65862222f697fea3d0139e
Lucas Albuquerque <luucas.albuquerque23@gmail.com>
2025-02-01 11:01:24 -0300

    initial of project  application dockerfile

---
+++ src/main/java/com/aulapos/minha_aplicacao/AplicacaoWebApplication.java
@@ -0,0 +1,13 @@
+package com.aulapos.minha_aplicacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacaoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacaoWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aulapos/minha_aplicacao/AplicacaoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aulapos.minha_aplicacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacaoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
